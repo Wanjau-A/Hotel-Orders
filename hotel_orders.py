b23168b0d8f3a184426a8cc94f98f30f66bf862d
nival350 <157099376+nival350@users.noreply.github.com>
2024-09-09 09:38:18 +0300

    first commit

---
+++ hotel_orders.py
@@ -0,0 +1,41 @@
+MENU = {
+    "Fries masala and Sausage": 350,
+    "Full chicken": 700,
+    "Mukimo and Beef": 850,
+    "Kienyeji fry and Ugali": 1100,
+    "Samosa": 50,
+    "Pilau Beef": 550,
+    "Chapati quarter Chicken": 950,
+    "Taco": 300,
+    "Smocha": 100
+    "Smoothie": 300
+}
+
+
+def main():
+    orders = []
+    while True:
+        try:
+            order = input("Place an order (one at a time or ctrl Z to finish): ").strip().title() 
+            if order in MENU:
+                orders.append(order)
+            else:
+                print("The item you entered is not in menu!")
+        except EOFError:
+            print(f"Your order: {orders} ")
+            break
+        finally:
+          total = compute_total(orders)
+          print(f"Your total bill is ksh.{total}")
+    
+def compute_total(orders):
+    total = 0
+    for order in orders:
+        total += MENU[order]
+    return total
+
+
+
+
+if __name__ == "__main__":
+    main()
